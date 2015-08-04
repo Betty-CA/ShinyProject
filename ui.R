0a4af2d2231fcad94932a440180b318512f9a9f1
Betty-CA <chongchongma@gmail.com>
2015-08-05 03:37:17 +0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,30 @@
+shinyUI(
+  pageWithSidebar(
+    headerPanel("Diebets Prediction"),
+    
+    sidebarPanel(
+      textInput("name","Enter you name:"),
+      radioButtons("gender","Your gender:", c("Male" = "Male" ,"Female" = "Female" )),
+      
+      checkboxGroupInput("outputType","Select Predictors:", c("Height" = "height", "BMI" = "BMI" ), selected = "height"),
+      numericInput("age","Age",15, min=0, max=100,step=1),
+      numericInput("height","Height (cm)",150, min=100, max=200,step=5 ),
+      sliderInput("weight", "Weight (kg)", value =30, min = 10, max = 200, step = 2 ),
+      
+      submitButton("Submit")
+     
+      
+    ),
+    
+    mainPanel(
+      h3("Results of Prediction"),
+      h4("You entered"),
+      verbatimTextOutput("inputValue"),
+      h4("Which resulted in a prediction of"),
+      verbatimTextOutput("predition"),
+      
+      plotOutput("compareHeight")
+    )
+    
+  )
+)
